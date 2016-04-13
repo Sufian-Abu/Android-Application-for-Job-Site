ecdc0dd79f2f579f54bbf941a978ee12672e4982
Abu Sufian <ynor7005326@gmail.com>
2016-04-13 18:42:09 +0600

    changing JobCueAndroid

---
+++ JobCueAndroid/app/src/main/java/com/example/abusufian/jobcueandroid/JobSearch.java
@@ -0,0 +1,223 @@
+package com.example.abusufian.jobcueandroid;
+
+import android.app.ActionBar;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class JobSearch extends AppCompatActivity implements
+        ActionBar.OnNavigationListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+
+
+        //TODO TRYING TO ADD DropDown from here
+//        // Action Bar
+//
+//        // Adapter
+//        SpinnerAdapter adapter =
+//                ArrayAdapter.createFromResource(this, R.array.actions,
+//                        android.R.layout.simple_spinner_dropdown_item);
+//        // Callback
+//        ActionBar.OnNavigationListener callback = new ActionBar.OnNavigationListener() {
+//
+//            String[] items = getResources().getStringArray(R.array.actions); // List items from res
+//
+//            @Override
+//            public boolean onNavigationItemSelected(int position, long id) {
+//
+//                // Do stuff when navigation item is selected
+//
+//                Log.d("NavigationItemSelected", items[position]); // Debug
+//
+//                return true;
+//
+//            }
+//
+//        };
+//
+//        ActionBar actions = getActionBar();
+//        actions.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST); //null pointer exception
+//        actions.setDisplayShowTitleEnabled(false);
+//        actions.setListNavigationCallbacks(adapter, callback);
+//
+//
+
+        //TODO END
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        return false;
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Map";
+                case 1:
+                    return "Job Type";
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_job_search, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+
+            LinearLayout map = (LinearLayout)rootView.findViewById(R.id.Job_map);
+            LinearLayout jobType = (LinearLayout)rootView.findViewById(R.id.Job_type);
+
+
+            if(getArguments().getInt(ARG_SECTION_NUMBER) == 1)
+            {
+                map.setVisibility(View.VISIBLE);
+                jobType.setVisibility(View.GONE);
+            }
+            else
+            {
+                map.setVisibility(View.GONE);
+                jobType.setVisibility(View.VISIBLE);
+            }
+
+            return rootView;
+        }
+    }
+}
--- JobCueAndroid/app/src/main/java/com/example/abusufian/jobcueandroid/MainActivity.java
+++ JobCueAndroid/app/src/main/java/com/example/abusufian/jobcueandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.abusufian.jobcueandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button button = (Button)findViewById(R.id.button);
+        final Intent i = new Intent(this, JobSearch.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
